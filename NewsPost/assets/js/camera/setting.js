eca56a427168409b353ee01abd66c034a23b18f0
Soomin Cho <soomincho1@Soos-MacBook-Air.local>
2020-12-31 02:34:04 -0600

    file

---
+++ NewsPost/assets/js/camera/setting.js
@@ -0,0 +1,12 @@
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_2').camera({
+				//height: '400px',
+				loader: 'bar',
+				pagination: false,
+				thumbnails: true
+			});
+
+			jQuery('#camera_thumbs').hide();
+			
+		});
